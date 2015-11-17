650265c5f4611fbed4637ec1aafce9de3b07d909
christopherrascoe <christopherrascoe@10.40.21.62>
2015-11-17 17:05:38 -0500

    They're done

--- src/org/usfirst/frc/team2415/robot/Robot.java
+++ src/org/usfirst/frc/team2415/robot/Robot.java
@@ -37,19 +37,22 @@ public class Robot extends SampleRobot {
     
     public void operatorControl() {
     	//Write code here
-    	
+    	while(true) {
+    		updateLeft();
+    		updateRight();
+    	}
     }
     
     public void updateLeft(){
     	/*This function will update the left motor according the the driver's input
     	 * on the controller.*/
-    	
+    	left.set(gamepad.leftY()+gamepad.leftX());
     }
     
     public void updateRight(){
     	/*This function will update the right motor according the the driver's input
     	 * on the controller.*/
-    	
+    	right.set(gamepad.leftY()-gamepad.leftX());
     }
     
     public void test() {
